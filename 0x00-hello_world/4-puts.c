0ccf4a7b74e6b9f82f6a3cc4b04b61ac64453c4b
Nal-Tay <iyanumaks@gmail.com>
2022-09-08 15:44:40 -0700

    learning

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *  * Print using put 
+ *
+ *   * Must Return = 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
