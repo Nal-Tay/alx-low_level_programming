db6cc38102fef04b55e83b09517632a5dd105db7
Nal-Tay <iyanumaks@gmail.com>
2022-09-22 03:59:43 +0000

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,16 +20,16 @@ void print_buffer(char *b, int size)
 			printf("%.8x:", j);
 			for (k = j; k < j + 10; k++)
 			{
-			if (k % 2 == 0)
-				printf(" ");
-			if (k < size)
-				printf("%.2x", *(b + k));
-			else
-				printf("  ");
-			}	
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
 			printf(" ");
 			for (l = j; l < j + 10; l++)
-			{	
+			{
 				if (l >= size)
 					break;
 				if (*(b + l) < 32 || *(b + l) > 126)
@@ -38,6 +38,6 @@ void print_buffer(char *b, int size)
 					printf("%c", *(b + l));
 			}
 			printf("\n");
-		}	
+		}
 	}
 }
