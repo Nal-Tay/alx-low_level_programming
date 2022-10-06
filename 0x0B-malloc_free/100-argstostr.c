47d68515e9c97cd95913687fa241254acb890aea
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2022-10-06 05:42:00 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,7 +9,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i, n, k = 0, len = 0;
+	int i, n, k = 0, len = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
@@ -19,7 +19,7 @@ int i, n, k = 0, len = 0;
 	{
 		for (n = 0; av[i][n]; n++)
 			len++;
-}
+	}
 	len += ac;
 
 	str = malloc(sizeof(char) * len + 1);
@@ -28,11 +28,11 @@ int i, n, k = 0, len = 0;
 
 	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)
+		for (n = 0; av[i][n]; n++)
 		{
 			str[k] = av[i][n];
 			k++;
-	}
+		}
 		if (str[k] == '\0')
 		{
 		str[k++] = '\n';
