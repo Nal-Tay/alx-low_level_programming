ac6f22a6748c4f403476d86bcfbe9fd68c2e6c05
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2022-12-16 05:24:56 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/teddy004/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
